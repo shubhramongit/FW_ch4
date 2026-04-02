5322d84c8f2db3d0228e2501beae8c774cfe24f7
shubhram <shubhramjs978@gmail.com>
2026-04-02 11:57:21 +0530

    initialcommit

---
+++ src/components/PostList.jsx
@@ -0,0 +1,26 @@
+import { Fragment } from 'react'
+import PropTypes from 'prop-types'
+import { Post } from './Post.jsx'
+
+export function PostList({ posts = [] }) {
+return (
+<div>
+    {posts.map((post) => (
+        <Fragment key={post._id}>
+            <Post {...post}/>
+            <hr/>
+        </Fragment>    ))}
+    </div>
+)}
+<Post
+ title={Post.title}
+ author={Post.author}
+ contents={Post.contents}/>
+ PostList.propTypes={
+    posts:PropTypes.arrayOf(PropTypes.shape(Post.propTypes)).isRequired
+}
+
+
+
+
+
